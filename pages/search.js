30d03cacca4a80513e6c4f7b738d0bca02643700
Brice <bricel67@gmail.com>
2022-08-05 15:45:19 +0200

    get data from google api and create mockData

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../Response";
 
-const Search = () => {
+const Search = ({ results }) => {
+  console.log("results =>", results);
   return (
     <div>
       <Head>
@@ -18,3 +20,20 @@ const Search = () => {
 };
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const mockData = true;
+
+  const data = mockData
+    ? Response
+    : await fetch(`
+        https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+        context.query.searchType && "&searchType=image"
+      }`).then((res) => res.json());
+
+  return {
+    props: { results: data },
+  };
+}
