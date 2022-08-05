9c56b617921888b22bab147bbcafc22d53adbf12
Brice <bricel67@gmail.com>
2022-08-05 10:18:12 +0200

    create search header component

---
+++ components/SearchHeader.js
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
+import User from "./User";
+
+const SearchHeader = () => {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+
+    const term = searchInputRef.current.value;
+
+    if (!term.trim()) return;
+
+    router.push(`/search?term=${term.trim()}`);
+  };
+
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          onClick={() => router.push("/")}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          alt="google logo"
+          width="120"
+          height="40"
+          objectFit="contain"
+          className="cursor-pointer"
+        />
+
+        <form className="flex items-center border border-gray-200 rounded-full shadow-md px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl">
+          <input
+            className="w-full focus:outline-none "
+            type="text"
+            defaultValue={router.query.term}
+            ref={searchInputRef}
+          />
+          <XIcon
+            onClick={() => (searchInputRef.current.value = "")}
+            className="h-7 text-gray-500 cursor-pointer sm:mr-2"
+          />
+          <MicrophoneIcon className="h-6 hidden sm:inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 mr-2 " />
+          <SearchIcon className="h-6 hidden sm:inline-flex text-blue-500" />
+          <button onClick={search} type="submit" hidden></button>
+        </form>
+        <User className="ml-auto whitespace-nowrap" />
+      </div>
+    </header>
+  );
+};
+
+export default SearchHeader;
